13704e204b39556e6e624caf44ae2f4b9153692b
Quentin <51925676+couaky@users.noreply.github.com>
2023-10-13 22:03:59 +0200

    Better explosions with scale

--- components/Explosion.gd
+++ components/Explosion.gd
@@ -3,22 +3,18 @@ extends Node2D
 
 
 func shell_explosion():
-	$Sprite.scale = Vector2(0.2, 0.2)
 	$AnimationPlayer.play("ShellExplosion")
 
 
 func asteroid_explosion():
-	$Sprite.scale = Vector2(0.3, 0.3)
 	$AnimationPlayer.play("AsteroidExplosion")
 
 
 func cannon_fire():
-	$Sprite.scale = Vector2(0.1, 0.1)
-	$AnimationPlayer.play("ShellExplosion")
+	$AnimationPlayer.play("ShellFire")
 
 
 func torpedo_explosion():
-	$Sprite.scale = Vector2(0.8, 0.8)
 	$AnimationPlayer.play("TorpedoExplosion")
 
 
